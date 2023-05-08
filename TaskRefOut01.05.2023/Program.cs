25a21314db5c2daa414cb68832fdd224e8935b1c
hikmetbabayev <hikmetbabayev319@gmail.com>
2023-05-08 13:00:35 +0400

    Add project files.

---
+++ TaskRefOut01.05.2023/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TaskRefOut01._05._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Age = 18;
+            person.Name = "Hikmet";
+            Modify(ref person);
+
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+        }
+
+        static void Modify(ref Person person)
+        {
+            person.Age += 1;
+            person.Name = "HikmetBabayev";
+        }
+        class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+    }
+}
